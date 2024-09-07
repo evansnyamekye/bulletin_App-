0762964fd3bf34de969042f5ee968a0124b84943
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-09-07 05:45:07 +0000

    Update: a modify config has been applied to the new robocop linters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 
-# ApplicationHelper provides helper methods for views.
 module ApplicationHelper
-  # Your helper methods go here
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# ApplicationMailer is the base class for all mailers in the application.
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
