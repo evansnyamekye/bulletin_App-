4f2474da6f462895a82e24419afb44834cfe77f0
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-09-08 22:33:19 +0000

    applied application record with modified functionalities

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# ApplicationRecord is the base class for all models in the application.
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module BulletinApp
   # Application is the main class for the Rails application.
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.1
 
     # Configuration for the application, engines, and railties goes here.
     #
